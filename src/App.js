90e182924b1d4b2369ad5e022ef81960250f3af0
Gaurav <gauravsingh23699@gmail.com>
2025-06-17 23:32:54 +0530

    Create- Router,Routes, Route, Link

--- src/App.js
+++ src/App.js
@@ -4,12 +4,32 @@ import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import MoviesGrid from "./components/MoviesGrid";
+import Watchlist from "./components/Watchlist";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header></Header>
+
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/watchlist">Watchlist</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid />}></Route>
+            <Route path="/watchlist" element={<MoviesGrid />}></Route>
+          </Routes>
+        </Router>
+
         <MoviesGrid></MoviesGrid>
       </div>
 
